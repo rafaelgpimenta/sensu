cba5c683a30eed1f71cdca6cb2a01e44adb2d6ca
root <rafaelgpimenta@gmail.com>
2017-10-16 23:20:53 -0200

    set file format to unix

--- sensu.sh
+++ sensu.sh
@@ -1,57 +1,58 @@
-﻿echo "Install and Configure RabbitMQ"
+#!/bin/bash
+echo "Install and Configure RabbitMQ"
 sleep 3
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-sudo dpkg -i erlang-solutions_1.0_all.deb
-wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add -
-sudo apt-get update -y
-sudo apt-get install socat erlang-nox -y
+dpkg -i erlang-solutions_1.0_all.deb
+wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | apt-key add -
+apt-get update -y
+apt-get install socat erlang-nox -y
 wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server_3.6.10-1_all.deb
-sudo dpkg -i rabbitmq-server_3.6.10-1_all.deb
-sudo apt-get update -y
-sudo apt-get install rabbitmq-server -y
-sudo systemctl start rabbitmq-server
-sudo systemctl enable rabbitmq-server
-sudo rabbitmqctl add_vhost /sensu
-sudo rabbitmqctl add_user sensu senha
-sudo rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
+dpkg -i rabbitmq-server_3.6.10-1_all.deb
+apt-get update -y
+apt-get install rabbitmq-server -y
+systemctl start rabbitmq-server
+systemctl enable rabbitmq-server
+rabbitmqctl add_vhost /sensu
+rabbitmqctl add_user sensu senha
+rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
 
 echo "Install Redis Server"
 sleep 3
-sudo apt-get install redis-server apt-transport-https -y
-sudo systemctl start Redis-server
-sudo systemctl enable Redis-server
+apt-get install redis-server apt-transport-https -y
+systemctl start Redis-server
+systemctl enable Redis-server
 
 echo "Install and Configure Sensu"
 sleep 3
-wget -O- https://sensu.global.ssl.fastly.net/apt/pubkey.gpg | sudo apt-key add -
+wget -O- https://sensu.global.ssl.fastly.net/apt/pubkey.gpg | apt-key add -
 cp config/sensu.list /etc/apt/sources.list.d/
-sudo apt-get update -y
-sudo apt-get install sensu -y
+apt-get update -y
+apt-get install sensu -y
 cp config/rabbitmq.json /etc/sensu/conf.d/
 cp config/redis.json /etc/sensu/conf.d/
 cp config/api.json /etc/sensu/conf.d/
 
 echo "Install and Configure Sensu Dashboard"
 sleep 3
-wget -O- https://sensu.global.ssl.fastly.net/apt/pubkey.gpg | sudo apt-key add -
+wget -O- https://sensu.global.ssl.fastly.net/apt/pubkey.gpg | apt-key add -
 cp config/uchiwa.list /etc/apt/sources.list.d/
-sudo apt-get update -y
-sudo apt-get install uchiwa -y
-cp config/uchiwa.json /etc/sensu/conf.d/
-sudo systemctl start sensu-server
-sudo systemctl enable sensu-server
-sudo systemctl start sensu-api
-sudo systemctl enable sensu-api
-sudo systemctl start sensu-client
-sudo systemctl enable sensu-client
-sudo systemctl start uchiwa
-sudo systemctl enable uchiwa
+apt-get update -y
+apt-get install uchiwa -y
+cp config/uchiwa.json /etc/sensu/
+systemctl start sensu-server
+systemctl enable sensu-server
+systemctl start sensu-api
+systemctl enable sensu-api
+systemctl start sensu-client
+systemctl enable sensu-client
+systemctl start uchiwa
+systemctl enable uchiwa
 
 echo "Access Sensu Dashboard"
 sleep 3
-sudo apt-get install ufw -y
-sudo ufw enable
-sudo ufw allow 3000
+apt-get install ufw -y
+ufw enable
+ufw allow 3000
 cp config/client.json /etc/sensu/conf.d/
-sudo systemctl restart sensu-client
+systemctl restart sensu-client
 
